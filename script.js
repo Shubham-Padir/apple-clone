9074698b4b3016e1f1519f436b745ad1a25b1032
Shubham Padir <shubhamsunilpadirsp2003@gmail.com>
2024-05-27 14:24:06 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let blue = document.querySelectorAll(".cardsimg")
+let h4s = document.querySelectorAll(".cardsimg h4")
+blue.addEventListener("mousemove", function(){
+    h4s.style.color = "blue"
+})
+blue.addEventListener("mouseleave", function(){
+    h4s.style.color = "black"
+})
+
+
+
+
+// let heading1 = document.querySelector(".h51")
+// let heading2 = document.querySelector(".h52")
+// let imgfront = document.querySelector(".images")
+
+// heading1.addEventListener("click",function(){
+//     imgfront.style.display = "none"
+// })
+
+
+
